ca1f2222f88d811d2ea6e32faf07717edf537886
Barbara Mayfield <bmayfield1991@gmail.com>
2020-07-14 12:56:27 -0500

    work: single_number

--- single_number/single_number.py
+++ single_number/single_number.py
@@ -1,11 +1,30 @@
+from collections import Counter
+
 '''
 Input: a List of integers where every int except one shows up twice
 Returns: an integer
 '''
+# U
+# - We are inputing a list where every int shows up twice except ONE
+# - If number shows up more than one time it won't be returned
+# - Need to find a way to identify duplicates in a list and print the one that doesn't have a duplicate
+# - We are getting integers in return
+
+# P
+#   - Create a new list where the single int will be stored and returned
+#   - Look through the list and find the non-duped integer
+#   - If item matches another in the list, do not add it to the new list
+#   - We could also import Counter to find least-common element
+
+
+# E
 def single_number(arr):
     # Your code here
+    counter = Counter(arr)
+    return min(counter, key=counter.get)
 
-    pass
+
+# R
 
 
 if __name__ == '__main__':
